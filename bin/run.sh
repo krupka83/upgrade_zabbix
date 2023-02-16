1bfc5604cb038be74610744609ee036b2b2a78cb
krupka83 <46448228+krupka83@users.noreply.github.com>
2023-02-16 10:52:11 +0100

    Add files via upload

--- bin/run.sh
+++ bin/run.sh
@@ -27,6 +27,11 @@ read cesta
 WD=$cesta
 BACKUP=$WD/backup
 
+# vytvorit slozky pro soubory
+mkdir $WD/backup
+mkdir $WD/log
+mkdir $WD/run
+
 # nazev skriptu
 SCRIPT=Upgrade_zabbix_proxy
 
@@ -34,12 +39,16 @@ SCRIPT=Upgrade_zabbix_proxy
 DATE_LOG()
 {
     echo `date +%y/%m/%d_%H:%M:%S` $*
-}
+}git push
 
 DATE=`date +"%Y%m%d"`
 
 # promené
 OS=`cat /etc/os-release | grep -i NAME | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1`
+OS_verze=`cat /etc/os-release | grep -i VERSION_ID | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1`
+PG_release=`curl -s https://repo.zabbix.com/zabbix/$verze/rhel/$OS_verze/x86_64/ | grep -i zabbix-proxy-pgsql | tail -n 1 | cut -d '"' -f2`
+DEB_release=`curl -s https://repo.zabbix.com/zabbix/$verze/debian/pool/main/z/zabbix-release/ | grep -i _all*$OS_verze | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1`
+UBU_release=`curl -s https://repo.zabbix.com/zabbix/$verze/ubuntu/pool/main/z/zabbix-release/ | grep -i _all*$OS_verze | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1`
 
 # vyhledat yum
 yum=`which yum | cut -d '/' -f4`
@@ -49,7 +58,7 @@ if [[  $yum=yum  ]]
 then
 DB_yum=`yum list installed | less | grep -i zabbix-proxy | cut -d ' ' -f1`
 else
-DB_apt=`apt list --installed | grep -i zabbix-proxy | cut -d ' ' -f1`
+DB_apt=`apt list --installed | grep -i zabbix-proxy | cut -d ' ' -f1 | cut -d '/' -f1`
 fi
 
 # Logovani
@@ -101,7 +110,7 @@ case $OS in
 	#echo "##### $DATE_LOG : Smayani starych balicku #####" >> $LOG
 	
 	# pridat novy repoyitar
-	OS_verze=`cat /etc/os-release | grep -i VERSION_ID | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1`
+	#OS_verze=`cat /etc/os-release | grep -i VERSION_ID | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1`
 	case $OS_verze in
 		8)
 			if [[  $PG_ZABBIX=$DB_yum  ]]
@@ -150,26 +159,86 @@ case $OS in
 	echo "##### $DATE_LOG : Zjisteni verze zabbix-proxy #####" >> $LOG
 
     ;;	
-  Ubuntu) 
-    echo "BSD is a good system, too"
+  Ubuntu|Debian)
+	# stop zabbix
+        #`$zabbixstop`
+        service zabbix-proxy stop
+        sleep 10s
+        service zabbix-proxy status
+        echo "##### $DATE_LOG : Zastaveni sluzby zabbix-proxy #####" >> $LOG
+
+        # Zaloha DB+con
+        case $DB_apt in
+                $PG_ZABBIX)
+                        echo "##### $DATE_LOG : DB je POSTGRESQL #####" >> $LOG
+                        sudo -u postgres pg_dumpall > $BACKUP/db.sql
+                        echo "##### $DATE_LOG : Databaze jsou zalohovany #####" >> $LOG
+                        ;;
+                $MYSQL_ZABBIX)
+                        echo "##### $DATE_LOG : DB je MYSQL #####" >> $LOG
+                        mysqldump -u zabbix -pzabbix --all-databases > $BACKUP/db.sql
+                        echo "##### $DATE_LOG : Databaze jsou zalohovany #####" >> $LOG
+                        ;;
+        esac
+
+        # Smazat Repozitar
+        #echo "##### $DATE_LOG : Smazan istareho repozitare #####" >> $LOG
+        #rm -r /etc/yum.repos.d/zabbix.repo
+        #yum clean all
+        #echo "##### $DATE_LOG : Smayani starych balicku #####" >> $LOG
+
+        # pridat novy repoyitar
+        #OS_verze=`cat /etc/os-release | grep -i VERSION_ID | cut -d '"' -f2 | head -n 1 | cut -d ' ' -f1`
+        case $OS_verze in
+                9|10|11|12|13)
+                        if [[  $PG_ZABBIX=$DB_apt  ]]
+                        then
+                                echo "##### $DATE_LOG : Stazeny repoyitar pro CentOS $OS_verze #####" >> $LOG
+                                rpm -Uvh https://repo.zabbix.com/zabbix/$verze/rhel/$OS_verze/x86_64/$PG_release
+                        else
+				echo "##### $DATE_LOG : Stazeny repoyitar pro CentOS $OS_verze #####"
+				wget https://repo.zabbix.com/zabbix/$verze/debian/pool/main/z/zabbix-release/$MYSQL_release
+                        fi
+                ;;
+                18.04|20.04|22.04|24.04|26.04)
+                        if [[  $PG_ZABBIX=$DB_yum  ]]
+                        then
+                                echo "##### $DATE_LOG : Stazeny repoyitar pro CentOS $OS_verze #####" >> $LOG
+                                rpm -Uvh https://repo.zabbix.com/zabbix/$verze/rhel/$OS_verze/x86_64/$PG_release
+                        else
+                                echo "mzsql"
+                        fi
+                ;;
+	       *)
+		echo "##### $DATE_LOG : OS verze linuxu nepodporuje tento script #####" >> $LOG
+		exit 0
+	;;
+        esac
+
+        # nacist package
+        apt update
+        echo "##### $DATE_LOG : Pridani novych balicku #####" >> $LOG
+
+        # Install Zabbix proxy
+        `$zabbixproxyinstall_apt`
+        echo "##### $DATE_LOG : Nainstalovany novy zabbix-proxy #####" >> $LOG
+
+        # Zabbix-proxy start
+        #`$zabbixstart`
+        service zabbix-proxy start
+        echo "##### $DATE_LOG : Spusteni zabbix-proxy #####" >> $LOG
+
+        # zjistit verzi
+        `$findver` >> $LOG
+        echo "##### $DATE_LOG : Zjisteni verze zabbix-proxy #####" >> $LOG
     ;;
   RedHat)
     echo "you should consider an open source system"
     ;;
-  DEBIAN)
-    echo "ted"
-    ;;    
   *)
     echo "##### $DATE_LOG : Server nepodporuje OS, doporucuji upgrade rucne #####" >> $LOG
     ;;
 esac
-
-# ceka na behnuti zabbix-proxy
-sleep 15s
-
-# Status sluzby
-`$zabbixstatus` >> $LOG
-#service zabbix-proxy status
 
 # skript probehl spravne
 echo 0 > $WD/run/$SCRIPT.rc
