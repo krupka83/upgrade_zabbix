cf8d8fe5f60b7e099ea323fd341917230cc799e8
krupka83 <46448228+krupka83@users.noreply.github.com>
2023-02-22 13:24:38 +0100

    Add files via upload

--- bin/run.sh
+++ bin/run.sh
@@ -167,7 +167,7 @@ case $OS in
 
     ;;	
   Ubuntu|Debian)
-	DEB_release=`curl -s https://repo.zabbix.com/zabbix/$verze/debian/pool/main/z/zabbix-release/ | grep -i "$OS_verze"04_all | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1`
+	DEB_release=`curl -s https://repo.zabbix.com/zabbix/$verze/debian/pool/main/z/zabbix-release/ | grep -i "$OS_verze"_all | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1`
 	UBU_release=`curl -s https://repo.zabbix.com/zabbix/$verze/ubuntu/pool/main/z/zabbix-release/ | grep -i "$OS_verze".04_all | tail -n 1 | cut -d '>' -f2 | cut -d '<' -f1`
 	# stop zabbix
         #`$zabbixstop`
